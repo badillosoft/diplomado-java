1bbbaa2473a8498a085fed0565a01e040c1b632a
Alan Badillo <badillo.soft@hotmail.com>
2017-10-28 15:20:51 -0500

    MII Sesión 2

---
+++ MII_Sesion2/src/cic/client/FileUploaderClient.java
@@ -0,0 +1,32 @@
+package cic.client;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class FileUploaderClient {
+    
+    Socket client;
+    
+    ArrayList<File> files;
+    
+    void refresh(String host, int port) throws IOException, ClassNotFoundException {
+        this.client = new Socket(host, port);
+        ObjectInputStream in = new ObjectInputStream(this.client.getInputStream());
+        
+        this.files = (ArrayList<File>)in.readObject();
+    }
+    
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        FileUploaderClient fuc = new FileUploaderClient();
+        
+        fuc.refresh("localhost", 2001);
+        
+        for (File f : fuc.files) {
+            System.out.println(f.getAbsolutePath());
+        }
+    }
+    
+}
